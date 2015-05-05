85cb0d80b3b6b94fc4ee13b95dd4d7d53a1636f3
Kirk Dietz <kdietz1@email.davenport.edu>
2015-05-05 08:38:53 -0400

    Added untracked files

---
+++ src/XPlaneGenConsole/Datapoint/CsvFieldAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XPlaneGenConsole
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public class CsvFieldAttribute : Attribute
+    {
+        public readonly int Index;
+        public readonly Type Type;
+
+        public CsvFieldAttribute(int index)
+        {
+            Index = index;
+        }
+
+        public CsvFieldAttribute(int index, Type type) : this(index)
+        {
+            Type = type;
+        }
+    }
+}
